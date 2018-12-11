62402e80e64314e5df3cacc309e88207d46bae59
MauTrib <45791775+MauTrib@users.noreply.github.com>
2018-12-11 15:07:15 +0100

    Update zz_VortexStreetMaggle.py

--- zz_VortexStreetMaggle.py
+++ zz_VortexStreetMaggle.py
@@ -1,34 +1,3 @@
-# -*- ucoding: utf-8 -*-
-"""
-LE SUPER PROJET D'INFO QUI EST TECHNIQUEMENT FINI MAIS QU'ON PEUT ENCORE PIMPER
-
-
-Trucs à faire pour la propreté du code :
-
-** LISIBILITE
- * commenter ce qui reste à commenter (lignes 400 et +)
- 
-** FEATURES
- * Mettre l'obstacles dans la fonction CL
- * donner des noms aux CL
- 
- * Ajouter une force (dans l'etape de pression ?)
-
- * calculer force cylindre
-
-Trucs à tenter quand tout marchera et sera propre :
- * Essayer différents obstacles => voir en changeant la forme de l'obstacle(carré,triangle, demi-cercle...), en en mettant plusieurs...
- * Essayer différentes CL
- * Tenter une interpolation bicubique au lieu de bilinéaire (et voire si du coup on peut baisser un peu la tolérance sur dt)
- ** Si VRAIMENT tout est ok :
-  * Rajouter l'advection de la température
-  * Rajouter la diffusion de la température
-  * Rajouter une source chaude
-  * Rajouter la force en fonction de la température -> RAYLEIGH BENAAAARD
-
-<3 sur toi qui as fait tout ça
-
-"""
 print("Début du programme...")
 
 ### IMPORTS
